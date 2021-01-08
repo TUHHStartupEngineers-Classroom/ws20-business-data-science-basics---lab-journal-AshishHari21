c9674049df9c399e88d5a679b154b59624f048da
AshishHari21 <ashishhari9999@gmail.com>
2021-01-08 15:47:00 +0100

    corrected

--- docs/solution/Patent.R
+++ docs/solution/Patent.R
@@ -1,21 +1,21 @@
-library(tidyverse)
-library(dplyr)
-library(vroom)
-library(tictoc)
-library(data.table)
+# library(tidyverse)
+# library(dplyr)
+# library(vroom)
+# library(tictoc)
+# library(data.table)
 
 # col_types <- cols_only(
   # id = col_character(),
-  # # type = col_character(),
-  # # number = col_character(),
-  # # country = col_character(),
+  # type = col_character(),
+  # number = col_character(),
+  # country = col_character(),
   # date = col_date("%d-%m-%y"),
-  # # abstract = col_character(),
-  # # title = col_character(),
-  # # kind = col_character(),
-  # # num_claims = col_double(),
-  # # filename = col_character(),
-  # # withdrawn = col_double()
+  # abstract = col_character(),
+  # title = col_character(),
+  # kind = col_character(),
+  # num_claims = col_double(),
+  # filename = col_character(),
+  # withdrawn = col_double()
 # )
 
 # patent_tbl <- vroom(
@@ -24,37 +24,37 @@ library(data.table)
   # col_types  = col_types,
   # na         = c("", "NA", "NULL")
 # )
-col_types_PA <- cols_only(
-  patent_id = col_character()
+# col_types_PA <- cols_only(
+  # patent_id = col_character()
   # assignee_id = col_character(),
   # location_id = col_character()
-  )
+  # )
 # patent_assignee_tbl <- vroom(
   # file       = "~/Data science/DS_101/00_data/patent/patent_assignee.tsv", 
   # delim      = "\t", 
   # col_types  = col_types_PA,
   # na         = c("", "NA", "NULL")
 # )
-# col_types_A <- cols_only(
-  # id = col_character(),
-  # # type = col_double(),
-  # # name_first = col_character(),
-  # # name_last = col_character(),
-  # organization = col_character()
+ # col_types_A <- cols_only(
+   # id = col_character(),
+   # type = col_double(),
+   # name_first = col_character(),
+   # name_last = col_character(),
+   # organization = col_character()
+ # )
+# assignee_tbl <- vroom(
+  # file       = "~/Data science/DS_101/00_data/patent/assignee.tsv", 
+  # delim      = "\t", 
+  # col_types  = col_types_A,
+  # na         = c("", "NA", "NULL")
 # )
-assignee_tbl <- vroom(
-  file       = "~/Data science/DS_101/00_data/patent/assignee.tsv", 
-  delim      = "\t", 
-  col_types  = col_types_A,
-  na         = c("", "NA", "NULL")
-)
 
-tic()
-assignee_tbl %>%
-  filter(!is.na(organization)) %>%
-  count(organization) %>%
-  arrange(desc(n))
-toc()
+# # gives the number of times a company is repeated in decending order. From which 
+# # we can get highest to lowest number of patient for each company
+# assignee_tbl %>%
+  # filter(!is.na(organization)) %>%
+  # count(organization) %>%
+  # arrange(desc(n))
 
 
 # class(patent_tbl)
@@ -69,3 +69,4 @@ toc()
                        # all.x = TRUE, 
                        # all.y = FALSE)
 # toc()
+
